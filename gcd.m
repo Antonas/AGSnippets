9d2dbc4b49108bd87319bc90324f4df687d0b1e7
Antonas <anton.as@me.com>
2014-03-02 14:32:38 +0100

    Imported user code snippets

---
+++ gcd.m
@@ -0,0 +1,13 @@
+// GCD
+// 
+//
+// IDECodeSnippetCompletionPrefix: gcd
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 59427DDF-84A8-400B-8651-AB0A8F81A3BC
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,
+                                             (unsigned long)NULL), ^(void) {
+        <#task#>
+    });
---
+++ gcd_main_thread.m
@@ -0,0 +1,12 @@
+// GCD Main Thread
+// 
+//
+// IDECodeSnippetCompletionPrefix: main thread
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 183E33CE-4787-4CE1-9164-EA3ADCF78753
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+		dispatch_async(dispatch_get_main_queue(), ^{
+
+        });
